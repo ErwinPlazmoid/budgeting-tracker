497b7a691f0b87f72afba309995e9f3bb341d5de
ErwinPlazmoid <olegbirds689@gmail.com>
2025-10-16 15:54:39 +0300

    fix(auth) template_names path

--- users/views.py
+++ users/views.py
@@ -5,13 +5,13 @@ from django.views.generic import CreateView
 
 class RegisterView(CreateView):
     form_class = UserCreationForm
-    template_name = "users/templates/register.html"
+    template_name = "users/register.html"
     success_url = reverse_lazy("login")
 
 class CustomLoginView(LoginView):
-    template_name = "users/templates/login.html"
+    template_name = "users/login.html"
     redirect_authenticated_user = True
 
 class CustomLogoutView(LogoutView):
-    template_name = "users/templates/logout.html"
+    template_name = "users/logout.html"
     next_page = reverse_lazy("login")  # redirect after logout
