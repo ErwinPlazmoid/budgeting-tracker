2fc55ef5f473b27712737671a3206f6fbd894816
ErwinPlazmoid <olegbirds689@gmail.com>
2025-10-16 15:43:50 +0300

    feat(auth): implement login, logout, and registration views using Django generic auth views

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name='login'),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("login/", auth_views.CustomLoginView.as_view(template_name="users/login.html"), name='login'),
+    path("logout/", auth_views.CustomLogoutView.as_view(), name="logout"),
     path("register/", views.RegisterView.as_view(), name="register"),
 
     #password management
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "users/templates/register.html"
+    success_url = reverse_lazy("login")
+
+class CustomLoginView(LoginView):
+    template_name = "users/templates/login.html"
+    redirect_authenticated_user = True
+
+class CustomLogoutView(LogoutView):
+    template_name = "users/templates/logout.html"
+    next_page = reverse_lazy("login")  # redirect after logout
